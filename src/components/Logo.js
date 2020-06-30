c872bfabfd4540949a60905dcb2598f3e069bacb
vikttu <nespolopeixoto@outlook.com>
2020-06-30 16:01:47 -0300

    changing the css class properties to javascript objects inside the react component Logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,10 +1,42 @@
 import React from 'react'
 
+
 function Logo() {
+    const iconTitle = 'VIKTOR NESPOLO PEIXOTO'
+    const subtTitle = 'WEB DEVELOPER'
+    const styles = {
+        borderStyle: 'solid',
+        borderColor: 'black',
+        borderWidth: '10px',
+        position: 'fixed',
+        backgroundColor: 'white',
+        top: '0',
+        left: '20vw',
+        bottom: '0',
+        right: '20vw',
+        padding: '1vw',
+        textAlign: 'center',
+        textJustify: 'interWord'
+    }
+    const titleStyle = {
+        fontSize: '11vw',
+        fontWeight: 'bold',
+        margin: '1vw',
+    }
+    const subTitleStyle = {
+        fontSize: '1.3vw',
+        fontWeight: 'normal',
+        margin: '1vw'
+    }
+
+    //    if () {
+
+  //  }
+
     return (
-    <div className="logo">
-        <p className="myTitle">VIKTOR<br />NESPOLO<br />PEIXOTO</p>
-        <p className="mySubtitle">SOFTWARE DEVELOPER</p>
+    <div style={styles}>
+        <p style={titleStyle}>{iconTitle}</p>
+        <p style={subTitleStyle}>{subtTitle}</p>
     </div>
     )
 }
