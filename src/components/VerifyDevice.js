5bbb45a5eeefc528f9651ba0e144b0655995c333
vikttu <nespolopeixoto@outlook.com>
2020-07-08 17:57:12 -0300

    VerifyDevice saved, but nothing new rly

---
+++ src/components/VerifyDevice.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function VerifyDevice(props){
+    return(
+        <footer>
+            <p>This is a footer</p>
+        </footer>
+    )
+}
+
+export default VerifyDevice
